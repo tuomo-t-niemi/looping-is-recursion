22ce54eb271fee0677bae541fdf08de5bfd04109
Tuomo Niemi <tuomo.t.niemi@luukku.com>
2015-04-09 21:13:14 +0300

    Exercises 1 - 3

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,13 +1,33 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (let [helper (fn [n k acc]
+                 (if (zero? k)
+                   acc
+                   (recur n (dec k) (* acc n))))]
+    (helper base exp 1)))
 
 (defn last-element [a-seq]
-  ":(")
+  (let [helper (fn [tmp sq]
+                 (if (empty? sq)
+                   tmp
+                   (recur (first sq) (rest sq))))]
+    (helper nil a-seq)))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (let [helper (fn [s1 s2]
+                 (cond
+                  (and (empty? s1) (empty? s2))
+                    true
+                  (empty? s1)
+                    false
+                  (empty? s2)
+                    false
+                  (= (first s1) (first s2))
+                    (recur (rest s1) (rest s2))
+                  :else
+                    false))]
+    (helper seq1 seq2)))
 
 (defn find-first-index [pred a-seq]
   ":(")
